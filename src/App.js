dac4509640825891a4834200b14e432f81750603
Sherzamon Muhiddinov <sh.muhiddinov@uic.group>
2023-05-31 02:16:49 +0500

    new react

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import "./App.css"
+import Header from "./components/common/header/Header"
+import {Route, Routes,BrowserRouter} from "react-router-dom";
+import About from "./components/about/About"
+import CourseHome from "./components/allcourses/CourseHome"
+import Team from "./components/team/Team"
+import Pricing from "./components/pricing/Pricing"
+import Blog from "./components/blog/Blog"
+import Contact from "./components/contact/Contact"
+import Footer from "./components/common/footer/Footer"
+import Home from "./components/home/Home"
+import CoursesCardDetail from "./components/course_detail/course_detail";
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Header />
+
+        <Routes>
+          <Route exact path='/' element={<Home/>} />
+          <Route exact path='/about' element={<About/>} />
+          <Route exact path='/courses' element={<CourseHome/>} />
+          <Route exact path='/courses/:id' element={<CoursesCardDetail/>} />
+          <Route exact path='/team' element={<Team/>} />
+          <Route exact path='/pricing' element={<Pricing/>} />
+          <Route exact path='/journal' element={<Blog/>} />
+          <Route exact path='/contact' element={<Contact/>} />
+
+
+
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/allcourses/CoursesCard.jsx
@@ -0,0 +1,62 @@
+import React from "react"
+import "./courses.css"
+import {Link} from "react-router-dom"
+import {useEffect, useState} from "react"
+
+function CoursesCard() {
+    // const
+    const baseURL = "http://127.0.0.1:8000/api/courses/"
+    const [courses, setCourses] = useState([])
+
+    useEffect(() => {
+        fetch(baseURL)
+            .then(res => res.json())
+            .then((results) => {
+                setCourses(results.results)
+
+            }, (error) => {
+                console.log(error)
+            })
+    }, [])
+
+    console.log(courses)
+    return (
+        <>
+            <section className='coursesCards'>
+                <div className='containercards'>
+                    {courses.map((val) => (
+                        <div className='coursesCarditems' key={val.id}>
+                            <img className={'course-icon'} src={val.icon} alt=''/>
+                            <h1 className={'course-title'}>{val.title}</h1>
+                            <div className='rate'>
+                                <i className='fa fa-star'></i>
+                                <i className='fa fa-star'></i>
+                                <i className='fa fa-star'></i>
+                                <i className='fa fa-star'></i>
+                                <i className='fa fa-star'></i>
+                                <label htmlFor=''>(5.0)</label>
+                            </div>
+                            <div className={'teacher'}>
+                                <img className={'teacher-icon'}   src={val.teacher_image} alt=''/>
+                                <h4>by {val.teacher_fistname} <br/>{val.teacher_lastname}</h4>
+
+                            </div>
+
+                            <span className={'time'}>2 hours</span>
+                            <h3 className={'price'}>
+                                {val.price} All Course / {val.price_per} per month
+                            </h3>
+                            <Link key={val.id} id={val.id} to={`/courses/${val.id}`}>
+                                <button className='course-btn'>ENROLL NOW !</button>
+                            </Link>
+
+
+                        </div>
+                    ))}
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default CoursesCard
